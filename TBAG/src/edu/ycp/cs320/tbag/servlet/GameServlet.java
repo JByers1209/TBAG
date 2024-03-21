d8cfda4fc3ea74427f23c89bbcab9f284fe9b2d6
Evonstein411 <evonstein@ycp.edu>
2024-03-21 16:28:33 -0400

    removed more extra files
    removed some extra 2a stuff removed ajax servlet for game servlet modified how index works trying to figure things out

--- TBAG/src/edu/ycp/cs320/tbag/servlet/GameServlet.java
+++ TBAG/src/edu/ycp/cs320/tbag/servlet/GameServlet.java
@@ -26,17 +26,8 @@ public class GameServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 	        throws ServletException, IOException {
 
-	    // Retrieve user input from the form
-	    String userInput = req.getParameter("userInput");
-
-	    // Process the user input
-	    String result = ""; // Process the input and get the result
-
-	    // Set the result as an attribute
-	    req.setAttribute("result", result);
-
-	    // Forward to view to render the result HTML document
 	    req.getRequestDispatcher("/_view/game.jsp").forward(req, resp);
+	    
 	}
 
 
