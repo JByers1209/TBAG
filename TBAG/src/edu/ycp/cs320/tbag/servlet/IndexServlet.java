adc2ba285718c63706def2839787837b03dfb2f9
onnaii <116903434+onnaii@users.noreply.github.com>
2024-03-23 21:26:15 -0400

    login

--- TBAG/src/edu/ycp/cs320/tbag/servlet/IndexServlet.java
+++ TBAG/src/edu/ycp/cs320/tbag/servlet/IndexServlet.java
@@ -18,8 +18,13 @@ public class IndexServlet extends HttpServlet {
 
         if ("Play".equals(function)) {
             req.getRequestDispatcher("/_view/game.jsp").forward(req, resp);
-        } else {
+        } 
+        else if("Log In!".equals(function)) {
+        	req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+        }
+        else {
         	req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
         }
+       
 	}
 }
--- TBAG/war/_view/resume.jsp
+++ TBAG/war/_view/resume.jsp
@@ -5,7 +5,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Pause Game</title>
-<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="styleSheet.css">
 </head>
 <body>
 
@@ -27,7 +27,5 @@
     <input type="submit" name= "function" value="Restart Game" class="submit"> 
 
     </div>
-
-
 </body>
 </html>
