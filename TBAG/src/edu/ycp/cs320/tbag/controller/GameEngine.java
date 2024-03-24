5de079d5736ce8f4ea5a33e4695280ddbabd3250
JByers1209 <josmbyers@gmail.com>
2024-03-24 16:46:34 -0400

    Functional room movement
    I hard-coded 9 rooms that you can successfully move around in. If you try to move to the wrong connections it tells you that you can't move in that direction.

--- TBAG/src/edu/ycp/cs320/tbag/controller/GameEngine.java
+++ TBAG/src/edu/ycp/cs320/tbag/controller/GameEngine.java
@@ -1,65 +1,13 @@
 package edu.ycp.cs320.tbag.controller;
 
 import edu.ycp.cs320.tbag.model.Game;
-import edu.ycp.cs320.tbag.model.Player;
-import edu.ycp.cs320.tbag.model.Room;
 
 public class GameEngine {
-    private Game game;
-    private String response;
-	
-	Room start = new Room("Start", "You are in the starter room!");
-	Room north = new Room("North", "The north room");
-	Room west = new Room("West", "The west room");
-	Room east = new Room("East", "The east room");
-	Room south = new Room("South", "The south room");
-	
-	Player player = new Player(start);
-	
-	Room currentRoom;
-	
-	boolean hasStarted = false;
 
-	//Activates initial starting data
-	public String start() {
-		
-		Room room = player.getCurrentRoom();
-		
-		if(hasStarted == true) {
-			return "Game has already started";
-		}else {
-			String startingText = room.getDescription();
-			hasStarted = true;
-			return startingText;
-		}
-	}
-   
+	Game game = new Game();
+	//Processes the users commands and controls the game
     public String processUserInput(String userInput) {
-        // Convert user input to lowercase for case-insensitive comparison
-        String input = userInput.toLowerCase();
-
-        // Process user input and return game response
-        if (input.equals("start")) {
-        	response = start();
-        }else if (input.equals("north")) {
-        	player.moveTo(north);
-        	response = "You move north.";
-        } else if (input.equals("south")) {
-        	player.moveTo(south);
-        	response = "You move south.";
-        } else if (input.equals("west")) {
-        	player.moveTo(west);
-        	response = "You move west.";
-        } else if (input.equals("east")) {
-        	player.moveTo(east);
-        	response = "You move east.";
-        } else if (input.equals("location")){
-        	currentRoom = player.getCurrentRoom();
-        	response = "Current Location: " + currentRoom.getName();
-        }else {
-            return "Invalid command.";
-        }
-        return response;
+    	return game.processUserInput(userInput);
     }
     
 }
