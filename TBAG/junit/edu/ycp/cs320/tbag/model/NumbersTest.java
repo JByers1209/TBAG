08fbf29e306b98b381b778d20fc57a34e80d0bb3
onnaii <116903434+onnaii@users.noreply.github.com>
2024-03-24 17:06:17 -0400

    login servlet

--- TBAG/junit/edu/ycp/cs320/tbag/model/NumbersTest.java
+++ TBAG/junit/edu/ycp/cs320/tbag/model/NumbersTest.java
@@ -10,34 +10,34 @@ import edu.ycp.cs320.tbag.model.Game;
 public class NumbersTest {
 	private Game model;
 	
-	@Before
-	public void setUp() {
-		model = new Game(null, null, null, null);
-	}
-	
-	@Test
-	public void testSet_Get_First() {
-		model.setFirst(10.0);
-		assertEquals(10, model.getFirst(), 0.001);
-	}
-	
-	@Test
-	public void testSet_Get_Second() {
-		model.setSecond(5.0);
-		assertEquals(5, model.getSecond(), 0.001);
-	}
-	
-	@Test
-	public void testSet_Get_Third() {
-		model.setThird(1.5);
-		assertEquals(1.5, model.getThird(), 0.001);
-	}
-	
-	@Test
-	public void testResult() {
-		model.setResult(4.6);
-		assertEquals(4.6, model.getResult(), 0.001);
-	}
-	
+//	@Before
+//	public void setUp() {
+//		model = new Game(null, null, null, null);
+//	}
+//	
+//	@Test
+//	public void testSet_Get_First() {
+//		model.setFirst(10.0);
+//		assertEquals(10, model.getFirst(), 0.001);
+//	}
+//	
+//	@Test
+//	public void testSet_Get_Second() {
+//		model.setSecond(5.0);
+//		assertEquals(5, model.getSecond(), 0.001);
+//	}
+//	
+//	@Test
+//	public void testSet_Get_Third() {
+//		model.setThird(1.5);
+//		assertEquals(1.5, model.getThird(), 0.001);
+//	}
+//	
+//	@Test
+//	public void testResult() {
+//		model.setResult(4.6);
+//		assertEquals(4.6, model.getResult(), 0.001);
+//	}
+//	
 
 }
--- TBAG/src/edu/ycp/cs320/tbag/servlet/MultiplyNumbersServlet.java
+++ TBAG/src/edu/ycp/cs320/tbag/servlet/MultiplyNumbersServlet.java
@@ -23,53 +23,54 @@ public class MultiplyNumbersServlet extends HttpServlet {
 		req.getRequestDispatcher("/_view/addNumbers.jsp").forward(req, resp);
 	}
 	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		
-		System.out.println("AddNumbers Servlet: doPost");
-		
-		// holds the error message text, if there is any
-		String errorMessage = null;
+//	@Override
+//	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+//			throws ServletException, IOException {
+//		
+//		System.out.println("AddNumbers Servlet: doPost");
+//		
+//		// holds the error message text, if there is any
+//		String errorMessage = null;
+	}
 		
 		// decode POSTed form parameters and dispatch to controller
-		Game model = new Game(null, null, null, null);
-		try {
-			model.setFirst(getDoubleFromParameter(req.getParameter("first")));
-			model.setSecond(getDoubleFromParameter(req.getParameter("second")));
+//		Game model = new Game(null, null, null, null);
+//		try {
+//			model.setFirst(getDoubleFromParameter(req.getParameter("first")));
+//			model.setSecond(getDoubleFromParameter(req.getParameter("second")));
+//
+//			// check for errors in the form data before using is in a calculation
+//			if (model.getFirst() == null || model.getSecond() == null) {
+//				errorMessage = "Please specify two numbers";
+//			}
+//			// otherwise, data is good, do the calculation
+//			// must create the controller each time, since it doesn't persist between POSTs
+//			// the view does not alter data, only controller methods should be used for that
+//			// thus, always call a controller method to operate on the data
+//			else {
+//				GameEngine controller = new GameEngine();
+//				model.setResult(controller.multiply(model.getFirst(), model.getSecond()));
+//				System.out.println(model.getResult());
+//			}
+//		} catch (NumberFormatException e) {
+//			errorMessage = "Invalid double";
+//		}
+//	
+//		// add result objects as attributes
+//		// this adds the errorMessage text and the result to the response
+//		req.setAttribute("model", model);
+//		req.setAttribute("errorMessage", errorMessage);
+//		
+//		// Forward to view to render the result HTML document
+//		req.getRequestDispatcher("/_view/multiplyNumbers.jsp").forward(req, resp);
+//	}
+//
+//	// gets double from the request with attribute named s
+//	private Double getDoubleFromParameter(String s) {
+//		if (s == null || s.equals("")) {
+//			return null;
+//		} else {
+//			return Double.parseDouble(s);
+//		}
+//	}
 
-			// check for errors in the form data before using is in a calculation
-			if (model.getFirst() == null || model.getSecond() == null) {
-				errorMessage = "Please specify two numbers";
-			}
-			// otherwise, data is good, do the calculation
-			// must create the controller each time, since it doesn't persist between POSTs
-			// the view does not alter data, only controller methods should be used for that
-			// thus, always call a controller method to operate on the data
-			else {
-				GameEngine controller = new GameEngine();
-				model.setResult(controller.multiply(model.getFirst(), model.getSecond()));
-				System.out.println(model.getResult());
-			}
-		} catch (NumberFormatException e) {
-			errorMessage = "Invalid double";
-		}
-	
-		// add result objects as attributes
-		// this adds the errorMessage text and the result to the response
-		req.setAttribute("model", model);
-		req.setAttribute("errorMessage", errorMessage);
-		
-		// Forward to view to render the result HTML document
-		req.getRequestDispatcher("/_view/multiplyNumbers.jsp").forward(req, resp);
-	}
-
-	// gets double from the request with attribute named s
-	private Double getDoubleFromParameter(String s) {
-		if (s == null || s.equals("")) {
-			return null;
-		} else {
-			return Double.parseDouble(s);
-		}
-	}
-}
