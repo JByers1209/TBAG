e0102e9d80075dc10b95aa91dc72670cf7b5bf39
Ren <karendealva0@gmail.com>
2024-04-17 15:25:29 -0400

    updates to login servlet
    attempting to connect login pages to the database

--- TBAG/src/edu/ycp/cs320/tbag/servlet/LoginServlet.java
+++ TBAG/src/edu/ycp/cs320/tbag/servlet/LoginServlet.java
@@ -6,6 +6,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
 
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -14,9 +19,12 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        // Retrieve username and password from the request parameters
+        // Retrieve username and password 
         String username = req.getParameter("username");
         String password = req.getParameter("password");
+       
+        
+        
 
         // Check if username and password are valid
         if (isValidLogin(username, password)) {
