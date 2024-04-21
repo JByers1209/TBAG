127de080067c5a28cccd28e077a7f172c17c9510
JByers1209 <josmbyers@gmail.com>
2024-04-21 16:00:37 -0400

    working on getting ethans code.

--- TBAG/src/edu/ycp/cs320/tbag/model/NPC.java
+++ TBAG/src/edu/ycp/cs320/tbag/model/NPC.java
@@ -1,6 +1,11 @@
 package edu.ycp.cs320.tbag.model;
 
 public class NPC extends Actor{
+	
+	
+	public NPC(int actorID, int roomID, String name, int level, int xp, int maxHealth, int currentHealth) {
+    	super(actorID, roomID, name, level, xp, maxHealth, currentHealth);
+    }
 
 	public NPC(int maxHealth, Room room) {
 		super(maxHealth, room);
@@ -14,13 +19,5 @@ public class NPC extends Actor{
 	public NPC() {
 		super();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
